88a17b91bd60b2bfc4eaf300b6e8b395904dd201
snakecharmer1024 <snakecharmr1024@gmail.com>
2015-09-13 22:53:19 -0700

    Updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is an art piece that uses a pre-trained recursive neural network to generate text in the style of the king james bible. It is served via nodejs and uses responsivevoice.js to speak the text aloud. Training and sampling the neural network is done via <a href="https://github.com/karpathy/char-rnn">char-rnn</a>
+
+Note: Please do not attempt to give money to or worship the webapp, no matter how nicely it asks you.
---
+++ holyinternet.js
@@ -0,0 +1,39 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+var marker = '--------------------------';
+
+
+app.get('/pray.to.me', function(req, res){
+	console.log('received prayer');
+	var primetext = req.query.prayer;
+	var command;
+  	if (primetext.length == 0)
+  	{
+  		res.send('');
+  	}
+   	exec("th sample.lua cv/model.t7 -gpuid -1 -primetext " + primetext, function(error, stdout, stderr){
+		console.log(stdout);
+		var index = stdout.indexOf(marker);
+		//console.log(index);
+		//console.log(primetext);
+		if (index > -1)
+		{
+			stdout = stdout.substring(index + marker.length + 5);
+		}
+		stdout = stdout.replace(/(\w* \d*:\d*)/g, '\n\n');
+		stdout = stdout.replace('¶', '');
+		stdout = stdout.replace('[', '');
+		stdout = stdout.replace(']', '');
+		stdout = stdout.substring(0,stdout.lastIndexOf('.') + 1);
+		//stdout = encodeURIComponent(stdout);
+		console.log(stdout);
+		res.send(stdout);
+  	});  		
+
+});
+
+app.listen(3000);
